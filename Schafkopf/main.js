e6263d8f336777c1e4e95654666252fa7e1c159d
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-03-31 18:08:00 +0200

    Seite initial eingerichtet

--- Schafkopf/main.js
+++ Schafkopf/main.js
@@ -1,11 +1,11 @@
 // Neuseelandreise Skript
 
-var map = L.map('map').setView([-39.195, 175.620], 5);
+var map = L.map('map').setView([47.271620080336895, 11.382055502245557], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-39.195, 175.620]).addTo(map)
-    .bindPopup('Tongariro Nationalpark')
+L.marker([47.271620080336895, 11.382055502245557]).addTo(map)
+    .bindPopup('Burenwirt')
     .openPopup();
